d112877d9b368fd39e8bff7c467b023a016845d4
jevonkagenou <felixalbertajevon@gmail.com>
2024-11-01 08:37:53 +0700

    update java

--- Triangle08.java
+++ Triangle08.java
@@ -7,27 +7,15 @@ public class Triangle08 {
         System.out.print("Masukkan Nilai N : ");
         int N = sc.nextInt();
         int i = 0;
-
-        // Salah
-        while (i <= N) {
+        while (i < N) {
             int j = 0;
-            while (j < i) {
+            while (j < i + 1) {
                 System.out.print("*");
                 j++;
             }
+            System.out.println();
             i++;
         }
-
-        // Benar
-        // while (i < N) {
-        //     int j = 0;
-        //     while (j < i + 1) {
-        //         System.out.print("*");
-        //         j++;
-        //     }
-        //     System.out.println();
-        //     i++;
-        // }
         sc.close();
     }
 }
